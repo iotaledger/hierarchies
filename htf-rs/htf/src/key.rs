0c2bed5a608b0e4acd47d53d060c25bb867b3c46
Yasir <yasirshariffa@gmail.com>
2024-08-26 16:36:37 +0300

    chore: removed unused keyid

--- htf-rs/htf/src/key.rs
+++ htf-rs/htf/src/key.rs
@@ -12,34 +12,6 @@ impl SignatureScheme for IotaKeySignature {
   type Signature = Vec<u8>;
 }
 
-/// Mirrored types from identity_storage::KeyId
-#[derive(Debug, Clone, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
-pub struct KeyId(String);
-
-impl KeyId {
-  /// Creates a new key identifier from a string.
-  pub fn new(id: impl Into<String>) -> Self {
-    Self(id.into())
-  }
-
-  /// Returns string representation of the key id.
-  pub fn as_str(&self) -> &str {
-    &self.0
-  }
-}
-
-impl std::fmt::Display for KeyId {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    f.write_str(&self.0)
-  }
-}
-
-impl From<KeyId> for String {
-  fn from(value: KeyId) -> Self {
-    value.0
-  }
-}
-
 #[derive(Clone)]
 pub struct SigningInfo {
   pub sender_public_key: Vec<u8>,
