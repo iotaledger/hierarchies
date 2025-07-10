543750964409e54e9fa952e9c88ee83db8dce88f
Yasir Shariff <yasir@shariff.dev>
2025-07-10 09:13:12 +0300

    refactor: update dependencies and improve code structure
    - Removed unused dependencies and updated existing ones in `Cargo.toml` files for better clarity and maintainability.
    - Enhanced the `Capability` enum by implementing the `Display` trait for improved string representation.
    - Cleaned up workspace configurations across multiple `Cargo.toml` files to ensure consistency and reduce redundancy.
    - Adjusted error handling in the `Error` enum by removing unnecessary attributes for a cleaner implementation.

--- ith.rs/ith/src/core/types/cap.rs
+++ ith.rs/ith/src/core/types/cap.rs
@@ -1,13 +1,23 @@
+use core::fmt;
+use std::fmt::Display;
+
 use serde::{Deserialize, Serialize};
 
 /// Capabilities are the different types of capabilities that can be issued
 /// to an account
-#[derive(Debug, strum::Display, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Capability {
-    #[strum(serialize = "RootAuthorityCap")]
     RootAuthority,
-    #[strum(serialize = "AttestCap")]
     Attest,
-    #[strum(serialize = "AccreditCap")]
     Accredit,
+}
+
+impl Display for Capability {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Capability::RootAuthority => write!(f, "RootAuthorityCap"),
+            Capability::Attest => write!(f, "AttestCap"),
+            Capability::Accredit => write!(f, "AccreditCap"),
+        }
+    }
 }
