106d4d5a925c46f6fa7b85a42367b3f614b1552f
Pawel Iwan <pawel.ivan@iota.org>
2025-07-21 13:27:30 +0200

    feat: expose properties of Accreditations and Accreditation

--- bindings/wasm/hierarchies_wasm/src/wasm_types/accreditations.rs
+++ bindings/wasm/hierarchies_wasm/src/wasm_types/accreditations.rs
@@ -4,10 +4,25 @@
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
+use crate::wasm_types::accreditation::WasmAccreditation;
+
 /// Represents a collection of accreditation statements
 #[wasm_bindgen(js_name = Accreditations, inspectable)]
-#[derive(Deserialize, Serialize, Clone)]
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq, Eq)]
 pub struct WasmAccreditations(pub(crate) ith::core::types::Accreditations);
+
+#[wasm_bindgen(js_class = Accreditations)]
+impl WasmAccreditations {
+    /// Returns the accreditations as an array.
+    #[wasm_bindgen(getter)]
+    pub fn statements(&self) -> js_sys::Array {
+        self.0
+            .statements
+            .iter()
+            .map(|accreditation| WasmAccreditation(accreditation.clone()).into())
+            .collect()
+    }
+}
 
 impl From<ith::core::types::Accreditations> for WasmAccreditations {
     fn from(value: ith::core::types::Accreditations) -> Self {
@@ -17,6 +32,6 @@ impl From<ith::core::types::Accreditations> for WasmAccreditations {
 
 impl From<WasmAccreditations> for ith::core::types::Accreditations {
     fn from(value: WasmAccreditations) -> Self {
-        serde_wasm_bindgen::from_value(value.into()).expect("From implementation works")
+        value.0
     }
 }
