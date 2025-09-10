9adac74d3637c2ff3718680f538301126059e0a7
Yasir <yasir@shariff.dev>
2025-09-10 13:53:58 +0300

    chore: standardize quotes and formatting in workflow and test files
    - Updated quotes in the GitHub Actions workflow for consistency.
    - Modified test files to ensure proper formatting and added missing newlines at the end of files.

--- bindings/wasm/hierarchies_wasm/examples/src/test.ts
+++ bindings/wasm/hierarchies_wasm/examples/src/test.ts
@@ -20,7 +20,7 @@ import { getProperties } from "./validation/03_get_properties";
 import { afterEach } from "mocha";
 
 // Only verifies that no uncaught exceptions are thrown, including syntax errors etc.
-describe("Test node examples", function () {
+describe("Test node examples", function() {
     afterEach(
         () => {
             console.log("\n----------------------------------------------------\n");
