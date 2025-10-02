55fe6a89996777ae457b0ffa8da57418ed7cba19
Yasir <yasir@shariff.dev>
2025-10-02 10:15:07 +0300

    chore: add node polyfills plugin to Vite configuration

--- bindings/wasm/hierarchies_wasm/cypress/app/vite.config.js
+++ bindings/wasm/hierarchies_wasm/cypress/app/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig(({ command, mode }) => {
     // variables will be set during build time
